c722d6227fb91ad46c6c8dd6dddef9086d783eaa
btiedeman <b.tiedeman@northwestern.edu>
2015-10-01 11:37:00 -0500

    Update and rename AutoloadExample.php to Ldap.php

---
+++ Ldap.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package mccwebdev
+ * @subpackage mccwebdev-yii2-ldap
+ * @version 1.0.0
+ */
+
+namespace mccwebdev\ldap;
+
+/**
+ * This is just an example.
+ */
+class Ldap extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
